4a6875a992b8f8c3fe9e2a11b217a435ce0e3205
Martino Bettucci <ceo@p2enjoy.studio>
2023-11-27 10:21:04 +0100

    WIP local.johndalia.com

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.get('/generate-url', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://local.johndalia.tech:${PORT}`);
 });
